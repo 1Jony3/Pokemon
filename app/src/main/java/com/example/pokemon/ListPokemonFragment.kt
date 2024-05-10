11110c7d0f2225dc8fb3f95cc433b17aa26646a1
jany <ezhbikovskaya@gmail.com>
2024-05-10 20:44:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pokemon/ListPokemonFragment.kt
@@ -0,0 +1,11 @@
+package com.example.pokemon
+
+import androidx.fragment.app.Fragment
+import com.example.pokemon.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ListPokemonFragment : Fragment(R.layout.fragment_list_pokemon) {
+
+
+}
